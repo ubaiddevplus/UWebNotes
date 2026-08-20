970d1db00f590d5d5e35fb7a04042412780ba837
ubaiddevplus <ubaiddevplus@gmail.com>
2026-08-20 08:55:19 +0000

    add some data

---
+++ HTML_Elements/data_Elements.js
@@ -0,0 +1,25 @@
+const h1 = [
+    // header
+    { element: "&lt;h1&gt; &lt;/h1&gt;", title: "h1 — The heading of the page" },
+
+    // main
+    // point1
+    {
+        point1_Title: "Example of h1 usage.",
+        // ex_card
+        code_type: "HTML",
+        code: "&lt;h1&gt; <strong>Heading</strong> &lt;/h1&gt;",
+        result: "Heading",
+    },
+
+    // point2
+    {
+        point1_Title: "Use one main <code>&lt;h1&gt;</code> per page.",
+        // table
+        table_Header: ["Do", "Don't", "Advice"],
+        table: [["&lt;h1&gt;Site title&lt;/h1&gt;", "&lt;h1&gt;Site title&lt;/h2&gt;", "&lt;h1&gt;Site title&lt;/h2&gt;"], ["&lt;h1&gt;Welcome to my site&lt;/h1&gt;", "&lt;h1&gt;&lt;/h1&gt", "Put meaningful text inside the heading."], ["Use one main &lt;h1&gt; per page", "Use multiple &lt;h1&gt; elements for every section", "Keep the main page heading unique and semantic."], ["Use &lt;h1&gt; for the top-level title", "Use &lt;h1&gt; for small paragraph text", "Use headings for structure, not styling."]],
+    },
+    // nav
+    //point3
+    { point3_title: "See also", links: ['<li><a href="headings.html">&lt;h2&gt;</a></li>', '<li><a href="#">&lt;p&gt;</a></li>', '<li><a href="#">&lt;ul&gt;</a></li>', '<li><a href="#">&lt;strong&gt;</a></li>'], link_read_more: '<a href="https://programmieren-kurs.massarueit.workers.dev/Week-1-Upload/week1-lecture.pdf"> To read about<strong>h1</strong> and more </a>' }
+];
--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const card_data = [
-    { title: "&lt;h1&gt; &lt;/h1&gt;", about: "The top-level heading in HTML, used for a page's primary title", read_link: '<a href="HTML_Elements/h1.html" class="card-link">Read More</a>' },
+    { title: "&lt;h1&gt; &lt;/h1&gt;", about: "The top-level heading in HTML, used for a page's primary title", read_link: '<a href="HTML_Elements/h1/h1.html" class="card-link">Read More</a>' },
     { title: "Heading", about: "Coming soon", read_link: '<a href="HTML_Elements/headings.html" class="card-link">Read More</a>' },
     { title: "&lt;p&gt; &lt;/p&gt;", about: "Coming soon", read_link: '<a href="HTML_Elements/p.html" class="card-link">Read More</a>' },
     { title: "&lt;a&gt; &lt;/a&gt;", about: "Coming soon", read_link: '<a href="HTML_Elements/a.html" class="card-link">Read More</a>' }
